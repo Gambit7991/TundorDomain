7a01876f1ef9efccb595d92c565e372fa42c6f28
Gambit7991 <102786495+Gambit7991@users.noreply.github.com>
2023-07-10 17:30:02 -0400

    Domain classes were updated

--- src/main/java/org/domain/models/userRoles/DomainStudentModel.java
+++ src/main/java/org/domain/models/userRoles/DomainStudentModel.java
@@ -5,50 +5,47 @@ import lombok.Getter;
 import org.domain.models.DomainUserModel;
 import org.domain.models.userRoles.accountInformation.DomainUserInfo;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Getter
 @EqualsAndHashCode(callSuper = false)
 public class DomainStudentModel extends DomainUserModel {
-    private final List <DomainTutorModel> domainTutorModels = new ArrayList<>();
+//    private final List <DomainTutorModel> domainTutorModels = new ArrayList<>();
     public DomainStudentModel(DomainUserInfo domainUserInfo, int id) {
         super(domainUserInfo, id);
     }
 
-    public void addTutor(DomainTutorModel domainTutorModel) {
-        domainTutorModels.add(domainTutorModel);
-    }
+//    public void addTutor(DomainTutorModel domainTutorModel) {
+//        domainTutorModels.add(domainTutorModel);
+//    }
 
-    public void removeTutor(DomainTutorModel domainTutorModel) {
-        domainTutorModels.remove(domainTutorModel);
-    }
+//    public void removeTutor(DomainTutorModel domainTutorModel) {
+//        domainTutorModels.remove(domainTutorModel);
+//    }
 
-    public List<DomainTutorModel> getDomainTutorModels(){
-        return this.domainTutorModels;
-    }
+//    public List<DomainTutorModel> getDomainTutorModels(){
+//        return this.domainTutorModels;
+//    }
 
-    public DomainTutorModel getTutorById(int id) {
-        return domainTutorModels.stream().filter(i-> i.getId() == id).toList().get(0);
-    }
+//    public DomainTutorModel getTutorById(int id) {
+//        return domainTutorModels.stream().filter(i-> i.getId() == id).toList().get(0);
+//    }
 
-    @Override
-    public String toString() {
-        return "DomainStudentModel{" +
-                "super=" + super.toString() +
-                "domainTutorModels=" + tutorsInformation() +
-                '}';
-    }
+//    @Override
+//    public String toString() {
+//        return "DomainStudentModel{" +
+//                "super=" + super.toString() +
+//                "domainTutorModels=" + tutorsInformation() +
+//                '}';
+//    }
     public String customToString(){
         return super.toString();
     }
 
-    private String tutorsInformation(){
-        StringBuilder information = new StringBuilder();
-        for (DomainTutorModel a: domainTutorModels) {
-            information.append(a.customToString());
-        };
-        return information.toString();
-    }
+//    private String tutorsInformation(){
+//        StringBuilder information = new StringBuilder();
+//        for (DomainTutorModel a: domainTutorModels) {
+//            information.append(a.customToString());
+//        };
+//        return information.toString();
+//    }
 
 }
