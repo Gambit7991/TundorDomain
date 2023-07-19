aeb53e90bb13fee7b9a8400eb4ad4460c861fcab
Gambit7991 <tim.ua.7991@gmail.com>
2023-07-19 05:48:22 -0400

    classes has been renamed

--- src/main/java/org/domain/models/Account.java
+++ src/main/java/org/domain/models/Account.java
@@ -15,9 +15,4 @@ public abstract class Account {
     private UUID id;
     private UserInfo info;
 
-    public Account(UserInfo info, UUID id) {
-        this.info = info;
-        this.id = id;
-    }
-
 }
--- src/main/java/org/domain/models/userRoles/Admin.java
+++ src/main/java/org/domain/models/userRoles/Admin.java
@@ -2,9 +2,6 @@ package org.domain.models.userRoles;
 
 import lombok.*;
 import org.domain.models.Account;
-import org.domain.models.userRoles.accountInformation.UserInfo;
-
-import java.util.UUID;
 
 @Builder
 @Getter
@@ -13,8 +10,4 @@ import java.util.UUID;
 @AllArgsConstructor
 public class Admin extends Account {
 
-
-    public Admin(UserInfo userInfo, UUID id) {
-        super(userInfo, id);
-    }
 }
